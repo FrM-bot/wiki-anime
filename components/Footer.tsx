c085b57aa2c88597d5241ff5c1d514160fb6b406
Franco M <damianmaciel0@gmail.com>
2022-09-19 16:50:34 -0300

    add share button

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,16 @@ import { Button, ButtonLink } from 'components/Button'
 import ArrowIcon from 'icons/ArrowIcon'
 
 const Footer = () => {
+  const sharePage = () => {
+    window.navigator.share({
+      url: 'https://anime-app-eight.vercel.app',
+      title: 'Wiki Anime'
+    })
+  }
   return (
     <footer className='relative'>
       <div className='absolute right-2 flex justify-end top-[-25%] z-10'>
-        <Button><ArrowIcon props={{ style: { transform: 'rotate(-90deg)' }, onClick: () => window.scroll(0, 0) }} /></Button>
+        <Button props={{ onClick: () => window.scroll(0, 0) }}><ArrowIcon props={{ style: { transform: 'rotate(-90deg)' } }} /></Button>
       </div>
       <div className='py-4 bg-secondary/70 rounded-md px-4 mb relative bottom-2'>
 
@@ -14,6 +20,9 @@ const Footer = () => {
         </div>
         <div className="flex justify-end">
           <ButtonLink href='mailto:damianmaciel0@gmail.com?subject = Feedback&body = Message' props={{ target: '_blank' }}>damianmaciel0@gmail.com</ButtonLink>
+        </div>
+        <div className='w-full flex justify-end mt-2'>
+          <Button props={{ onClick: () => sharePage() }}>Share</Button>
         </div>
       </div>
 
